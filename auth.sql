cc2359cfad3665055b1e0019f1c685fbd6d69b23
Aman Chourasiya <134651828+Amanmodi8293@users.noreply.github.com>
2025-07-04 00:38:28 +0530

    Add files via upload

---
+++ auth.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE `secure_auth`;
+
+USE `secure_auth`;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` varchar(20) DEFAULT 'user',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
